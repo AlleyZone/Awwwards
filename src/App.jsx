11e57e531e2e5a24de14da5be8e79fd4fd0b529d
Alley <llzeynolla@vk.com>
2024-11-25 22:08:39 +0500

    Hero section

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from "./components/Hero";
+
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
